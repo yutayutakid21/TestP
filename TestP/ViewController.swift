1c020cd50f66fea23d5b1a7f8ade38b7d166ad54
Fujio Nakatsuka <com.nifty.cbm01799>
2019-11-22 19:25:57 +0900

    var age = 0 を入れました。

--- TestP/ViewController.swift
+++ TestP/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    
+    var age = 0
+        
     var string = String()
     
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }

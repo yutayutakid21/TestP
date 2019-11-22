9bdef8af1ba9914800738ea13529cee5377b2fb9
Yuta Fujii <yutayutakid21@gmail.com>
2019-11-22 19:06:24 +0900

    変数追加

--- TestP/ViewController.swift
+++ TestP/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var string = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
